3913a6fcf2b3efe9a790c965c5a9d7f572efc598
Wiktor Dolecki <wiktor.dolecki@gmail.com>
2012-09-12 17:49:30 +0200

    Work in progress on twitter oauth authorization in python.

--- python/oauth/twitterApi.py
+++
@@ -1,6 +0,0 @@
-
-# Module for twitter API urls
-
-AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token=%s"
-REQUEST_TOKEN_RESOURCE = "api.twitter.com/oauth/request_token"
-ACCESS_TOKEN_RESOURCE = "api.twitter.com/oauth/access_token"
